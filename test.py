6d2c91312f8e07b61ab8cdfa6fed0834af477096
obogrelova <on.obogrelova@gmail.com>
2025-02-19 09:50:34 +0500

    Update file

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
 import pytest
 from main import count_vowels
 
-@pytest.mark.parametrize
+@pytest.mark.parametrize("test_input, expected", [
+    ("aeiouyAEIOUY", 12),
+    ("bcdfg", 0),
+    ("Hello World!", 3),
+    ("Python is fun!", 4),
+    ("", 0)
+])
+
+def test_count_vowels(test_input, expected):
+    assert count_vowels(test_input) == expected
